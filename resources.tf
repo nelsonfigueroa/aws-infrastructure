12c707cf7e31f1439d6115ea338fce85d8e6c835
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-02-18 19:38:35 -0800

    Began implementing custom vpc

--- resources.tf
+++ resources.tf
@@ -1,38 +1,39 @@
-resource "aws_vpc" "environment-example" {
+# create a VPC
+resource "aws_vpc" "blog-vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
-  tags {
-    Name = "terraform-aws-vpc-example"
-  }
 }
 
-# create subnets located in VPC created above
+# create public subnet
 
-resource "aws_subnet" "subnet1" {
-  cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block, 3 ,1)}"
-  vpc_id = "${aws_vpc.environment-example.id}"
+resource "aws_subnet" "blog-public-subnet" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id = "${aws_vpc.blog-vpc.id}"
   availability_zone = "us-west-1b"
 }
 
-resource "aws_subnet" "subnet2" {
-  cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block, 2 ,2)}"
-  vpc_id = "${aws_vpc.environment-example.id}"
+# create private subnet
+resource "aws_subnet" "blog-private-subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = "${aws_vpc.blog-vpc.id}"
   availability_zone = "us-west-1b"
 }
 
-# create a security group
+# TODO: add an internet gateway, or provision a NAT instance
 
-resource "aws_security_group" "subnetsecurity" {
-  vpc_id = "${aws_vpc.environment-example.id}"
+# TODO: create a security group (not modified for new infrstructure yet...)
 
-  ingress {
-    cidr_blocks = [
-      "${aws_vpc.environment-example.cidr_block}"
-    ]
+# resource "aws_security_group" "subnetsecurity" {
+#   vpc_id = "${aws_vpc.blog-vpc.id}"
 
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  }
-}
+#   ingress {
+#     cidr_blocks = [
+#       "${aws_vpc.blog-vpc.cidr_block}"
+#     ]
+
+#   from_port = 80
+#   to_port = 80
+#   protocol = "tcp"
+#   }
+# }
--- servers.tf
+++ servers.tf
@@ -1,32 +1,32 @@
 
-data "aws_ami" "ubuntu" {
-  most_recent = true # if multiple results come back from AMI search, use most recent
+# data "aws_ami" "ubuntu" {
+#   most_recent = true # if multiple results come back from AMI search, use most recent
 
-  # search filters for image
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
+#   # search filters for image
+#   filter {
+#     name = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+#   }
 
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  # owner of the image 'Canonical' in this case that distributes ubuntu
-  owners = ["099720109477"]
+#   # owner of the image 'Canonical' in this case that distributes ubuntu
+#   owners = ["099720109477"]
 
-}
+# }
 
-resource "aws_instance" "first_server" {
-  # take result from 'data' block aboce and assign to ami property of instance
-  ami = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+# resource "aws_instance" "first_server" {
+#   # take result from 'data' block aboce and assign to ami property of instance
+#   ami = "${data.aws_ami.ubuntu.id}"
+#   instance_type = "t2.micro"
 
-  tags {
-    Name = "identifiertag"
-  }
+#   tags {
+#     Name = "identifiertag"
+#   }
 
-  # assign this instance to subnet 2 created in resources.tf file
-  subnet_id = "${aws_subnet.subnet2.id}"
-}
+#   # assign this instance to subnet 2 created in resources.tf file
+#   subnet_id = "${aws_subnet.blog-private-subnet.id}"
+# }
