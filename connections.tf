9fdfc3df531dc90b22787d069812dabf599bda29
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-02-23 17:45:27 -0800

    Added name tags

--- connections.tf
+++ connections.tf
@@ -2,4 +2,3 @@ provider "aws" {
   # 'aws configure' set defaults for other needed information.
   region = "us-west-1"
 }
-
--- resources.tf
+++ resources.tf
@@ -3,6 +3,10 @@ resource "aws_vpc" "blog-vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
+  
+  tags = {
+    Name = "blog_vpc"
+  }
 }
 
 # create public subnet
@@ -11,6 +15,10 @@ resource "aws_subnet" "blog-public-subnet" {
   cidr_block = "10.0.0.0/24"
   vpc_id = "${aws_vpc.blog-vpc.id}"
   availability_zone = "us-west-1b"
+  
+  tags = {
+    Name = "blog_subnet_public"
+  }
 }
 
 # create private subnet
@@ -18,11 +26,18 @@ resource "aws_subnet" "blog-private-subnet" {
   cidr_block = "10.0.1.0/24"
   vpc_id = "${aws_vpc.blog-vpc.id}"
   availability_zone = "us-west-1b"
+  
+  tags = {
+    Name = "blog_subnet_private"
+  }
 }
+
+# TODO: route table? ACL? these get created by default with new VPC so idk.
+# ACL and Route Table are associated to VPC...check VPC description
 
 # TODO: add an internet gateway, or provision a NAT instance
 
-# TODO: create a security group (not modified for new infrstructure yet...)
+# TODO: create a security group
 
 # resource "aws_security_group" "subnetsecurity" {
 #   vpc_id = "${aws_vpc.blog-vpc.id}"
